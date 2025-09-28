48c47cecd8745279abb851c07f27615b0ee887b7
kelvengps7 <kelvengps7@gmail.com>
2025-09-28 15:51:45 +0000

    Conexão Github

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,9 +11,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    // Adiciona o plugin visualizer, que será executado ao criar o build
+    visualizer({
+      filename: "stats.html", // Nome do arquivo de saída do relatório
+      open: true, // Abre o relatório automaticamente no navegador após o build
+      gzipSize: true, // Mostra o tamanho dos arquivos após a compressão gzip
+      brotliSize: true, // Mostra o tamanho após a compressão brotli (mais eficiente)
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
